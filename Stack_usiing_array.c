4d4424bcee94bf7d6575033e0a4748399a306df9
Kangkan Paul <kool.kangkan@gmail.com>
2021-05-23 22:37:41 +0530

    Create Stack_usiing_array.c
    Implementation of Stack Data Structures using arrays

---
+++ Stack_usiing_array.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *head = NULL;
+
+void push(int val)
+{
+    //Task 1: Correct the below code
+    struct node *newNode = malloc(sizeof(struct node));
+    newNode->data = val;
+
+    newNode->next = head;
+    head = newNode;
+}
+
+int pop()
+{
+    //Implement Task 2 here
+    struct node *temp;
+    if(head == NULL){
+        return -1;
+    }
+    else{
+        temp = head;
+        head = head -> next;
+        return(temp -> data);
+        free(temp);
+    }
+}
+
+int main()
+{
+    push(10);
+    push(20);
+    push(30);
+    printf("%d ",pop());
+    printf("%d ",pop());
+    push(40);
+    printf("%d ",pop());
+    printf("%d ",pop());
+    printf("%d ",pop());
+
+    return 0;
+}
