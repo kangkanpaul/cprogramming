adda74de87ecb83ec8fdbd499137023ab30cd93c
Kangkan Paul <kool.kangkan@gmail.com>
2021-05-23 12:31:52 +0530

    Create Doubly_Linked_List.c
    Doubly Linked List Example in c

---
+++ Doubly_Linked_List.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    struct node
+    {
+        int data;
+        struct node* prev;
+        struct node* next;
+    };
+    struct node *head, *mid, *last;
+
+    head = malloc(sizeof(struct node));
+    mid  = malloc(sizeof(struct node));
+    last = malloc(sizeof(struct node));
+
+    head->data = 10;
+    mid->data = 20;
+    last->data = 30;
+
+    //Implement task 1 here
+    head -> prev = NULL;
+    head -> next = mid;
+    mid -> prev = head;
+    mid -> next = last;
+    last -> prev = mid;
+    last -> next = NULL;
+
+    //Implement task 2 here
+    
+    struct node *temp = last;
+
+    while(temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->prev;
+    }
+    
+    return 0;
+}
